2296c9020573ab9dbc8a1a687f0766ca0266d718
kristian andersen <krilleandersen@gmail.com>
2020-08-31 10:57:06 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ let asidenav=document.querySelector('.asidenav')
 let prev = document.querySelector('.prev');
 let next = document.querySelector('.next');
 
+
+
 let modalstate = 0;
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -71,7 +73,8 @@ function showtime() {
 
 
 
-let  date   = data.project[i].Date;
+//let  date   = data.project[i].Date;
+// <!--  <p>Date: ${date}<br>-->
 let  client = data.project[i].Client;
 let  role   = data.project[i].Role;
 let  uRL    = data.project[i].URL;
@@ -82,25 +85,46 @@ let  images = data.project[i].images;
 let  alts   = data.project[i].alts;
 let  titles = data.project[i].titles;
 let  fcaps  = data.project[i].fcaps;
+let  iframe  = data.project[i].iframe;
+let  motion  = data.project[i].motion;
 
 //======================================================================================//
 let modalslidecontent=`<section class="modal-content">
 <article>
     <aside class="info">
-        <p>Date: ${date}<br>
-            Client: ${client}<br>
+            <p>Client: ${client}<br>
             Role: ${role}<br>
-            URL: <a href="${uRL}">${uRLtxt}</a>
+            URL: <a href="${uRL}" target="_blank">${uRLtxt}</a>
         </p>
     </aside>
     <h2>${h2}</h2>
     <p>${ptxt}</p>
     <figure class="figure fadein">`
     
+    if(images.length>0){
+      console.log(images+'--'+images.length)
     for (let i = 0; i < images.length; i++) { 
       modalslidecontent=modalslidecontent+`<img class="center" src="${images[i]}" alt="${alts[i]}" title="${titles[i]}" loading="lazy" onload="className='fadein'" width="100%" height="auto">
        <figcaption>${fcaps[i]}</figcaption>`
     } 
+  }
+
+  if(iframe.length>0){
+    for (let i = 0; i < iframe.length; i++) { 
+      modalslidecontent=modalslidecontent+`<div class="icontainer"><iframe class='responsive-iframe' src="${iframe}"></div>"
+       <figcaption>${fcaps[i]}</figcaption>`
+    } 
+  }
+
+  if(motion.length>0){
+    for (let i = 0; i < motion.length; i++) { 
+      modalslidecontent=modalslidecontent+`<video width="850" controls>
+      <source src="${motion}" type="video/mp4">
+      Your browser does not support HTML video.
+    </video>
+       <figcaption>${fcaps[i]}</figcaption>`
+    } 
+  }
 
 modalslidecontent=modalslidecontent+`</figure></article></section>`
 modalslide.innerHTML += modalslidecontent;;
@@ -119,7 +143,10 @@ modalslides=document.querySelectorAll('.modal-content');
 
 
 
-
+  document.querySelector('.close').addEventListener('click', function(e) {
+    gsap.to(".modal", {duration: 0.8, xPercent: '100',ease: "elastic.In(1, 2)"});
+    modalstate = 0;
+  })
 
 
     document.querySelector('body').addEventListener('click', function(e) {
@@ -134,6 +161,7 @@ modalslides=document.querySelectorAll('.modal-content');
         if( modalstate == 0 ){
            modalstate = 1;
            asidenavgoto(e.target.id)
+    
            gsap.to(".modal", {duration: 0.8, xPercent: '-100',ease: "elastic.out(1, 2)"});
           
         }else{
@@ -160,17 +188,16 @@ modalslides=document.querySelectorAll('.modal-content');
 
 
 
-asidenav.addEventListener('click', function(e) {
+window.addEventListener('click', function(e) {
   // event.target is the clicked item
-  if (!e.target) { return; }
+  //if (!e.target) { return; }
 
+console.log("whaaaat")
 
-    let modalslides=document.querySelectorAll('.modal-content');
-    let curmf=modalslides[curentid].querySelectorAll('figure');
-    
-    //gsap.set(curmf, { display: block });
-    
     if (e.target.matches('.prev')) {
+      
+
+
       gsap.to(modalslides[curentid], {duration: 0.8, xPercent: 100,ease: "elastic.out(1, 2)"});
       
       if (curentid === 0) {
@@ -179,16 +206,24 @@ asidenav.addEventListener('click', function(e) {
         curentid--;
       }
 
+      let curmf=modalslides[curentid].querySelectorAll('figure');
+      gsap.set(curmf, { visibility: "visible" });
+
       gsap.fromTo(modalslides[curentid],0.8,{ xPercent: -100 },{ xPercent: 0 ,ease: "elastic.out(1, 2)"});
     }
 
     if (e.target.matches('.next')) {
-     gsap.to(modalslides[curentid], {duration: 0.8, xPercent: -100,ease: "elastic.out(1, 2)"});
+
+    
+      gsap.to(modalslides[curentid], {duration: 0.8, xPercent: -100,ease: "elastic.out(1, 2)"});
       if (curentid < modalslides.length - 1) {
         curentid++;
       } else {
         curentid = 0;
       }
+      let curmf=modalslides[curentid].querySelectorAll('figure');
+      gsap.set(curmf, { visibility: "visible" });
+
       gsap.fromTo(modalslides[curentid],0.8,{ xPercent: 100 },{ xPercent: 0 ,ease: "elastic.out(1, 2)"});
     }
  
@@ -203,7 +238,7 @@ asidenav.addEventListener('click', function(e) {
 
     let curmf=modalslides[brickid].querySelectorAll('figure');
     gsap.set(curmf, { visibility: "visible" });
-
+    modalslides[curentid].scrollTop=0;
       gsap.to(modalslides[curentid], {duration: 0.8, xPercent: -100,ease: "elastic.out(1, 2)"});
      
       curentid=brickid; 
